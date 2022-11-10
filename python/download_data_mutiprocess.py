0b40c1352387d9e889b885f5b505711d2e661157
LianShuaiLong <1091545654@qq.com>
2022-11-10 14:51:36 +0800

    add some mp pyfiles

--- python/download_data_mutiprocess.py
+++ python/download_data_mutiprocess.py
@@ -7,11 +7,12 @@ import requests
 import logging
 import base64
 
-
-
-def download_file(url):
-    img = requests.get(url)
-    return img
+def download_file(url,pic_name,dst_dir):
+    response = requests.get(url)
+    img = response.content
+    with open(f'{dst_dir}/{pic_name}','wb') as f:
+        f.write(img)
+    print(f'{dst_dir}/{pic_name} download successfully...')
 
 class FileReader(Process):
     def __init__(self, fpath, out_que: Queue, name: str):
@@ -19,65 +20,40 @@ class FileReader(Process):
         self.out_que = out_que
         self.name = name
         self.fpath = fpath
-        self.url = ''
-
+    
 
     def run(self):
-        with open(self.fpath, encoding='utf-8') as fin:
-            for line in fin:
-                self.url = line.strip('\n')
-                try:
-                   data = download_file(self.url)
-                except Exception:
-                    continue
-                data = base64.b64encode(data.content).decode("ascii")
-                self.out_que.put((self.url,data))
+        lines = open(self.fpath, encoding='utf-8').readlines()
+        for line in lines[1:]:
+            secs = line.strip('\n').split('\t')
+            self.url = secs[0][1:]
+            self.series_name = secs[1]
+            self.spec_name = secs[2]
+            self.pic_id = secs[3]
+            self.pic_name = '_'.join([self.series_name,self.spec_name,self.pic_id,'.jpg'])
+            self.pic_name = self.pic_name.replace(' ','')
+            self.url = 'https://car0.autoimg.cn'+self.url
+            self.out_que.put((self.url,self.pic_name))
 
-class FileProcessor(Process):
-    def __init__(self, in_que: Queue, out_que: Queue, stop_flag: Event, name: str):
+class FileDownloader(Process):
+    def __init__(self, in_que: Queue, out_que: Queue, stop_flag: Event, name: str,des_dir:str):
         Process.__init__(self)
         self.in_que = in_que
         self.out_que = out_que
         self.stop_flag = stop_flag
         self.name = name
+        self.des_dir = des_dir
     def run(self):
         while not (self.stop_flag.is_set() and self.in_que.empty()):
             fid = None
             try:
-                url,data = self.in_que.get(timeout=10)
+                url,pic_name= self.in_que.get(timeout=10)
+                download_file(url,pic_name,self.des_dir)
             except:
                 continue
-            res_dict = get_ocr_from_bml(data)
-            self.out_que.put((url,res_dict))
+            
+            
 
-
-class ResWriter(Process):
-    def __init__(self, out_dir, in_que: Queue, stop_flag: Event, name: str):
-        Process.__init__(self)
-        self.in_que = in_que
-        self.stop_flag = stop_flag
-        self.name = name
-        self.line_dict={}
-
-    def run(self):
-        while not (self.stop_flag.is_set() and self.in_que.empty()):
-            try:
-                url,res_dict = self.in_que.get(timeout=10)
-                self.line_dict[url] = res_dict
-            except:
-                continue
-
-            if len(self.line_dict) > 100:
-                self.flush2file()
-
-    def flush2file(self):
-        t = time.time()
-        f = open('edit_dist_0_with_keywords_{}.csv'.format(t), 'w')
-        for url, res_dict in self.line_dict.items():
-            res = [url,res_dict]
-            f.write('%s\n' % json.dumps(res))
-        f.close()
-        self.line_dict={}
 
 if __name__=='__main__':
     raw_que = Queue(100000)
@@ -88,15 +64,13 @@ if __name__=='__main__':
     global reg_done
     reg_done = Event()
 
-    pp1 = [FileReader('url_file.txt', raw_que, 'read_%d' % i) for i in range(1)]
-    pp2 = [FileProcessor(raw_que, res_que, read_done, 'reg_%d' % i) for i in range(32)]
-    pp3 = [ResWriter('rough_finder', res_que, reg_done, 'write_%d' % i) for i in range(1)]
+    pp1 = [FileReader('sql.res.log.1', raw_que, 'read_%d' % i) for i in range(1)]
+    pp2 = [FileDownloader(raw_que, res_que, read_done, 'reg_%d' % i,'data.autohome.1') for i in range(5)]
+
 
     for p in pp1:
         p.start()
     for p in pp2:
-        p.start()
-    for p in pp3:
         p.start()
 
     for p in pp1:
@@ -104,7 +78,5 @@ if __name__=='__main__':
     read_done.set()
     for p in pp2:
         p.join()
-    reg_done.set()
-    for p in pp3:
-        p.join()
+    
     print('all done')
